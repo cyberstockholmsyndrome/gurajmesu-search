3bbf317ab7dfc807d96565b4a2d82ea08c676e5c
boucherymusic <voignamirthecat@gmail.com>
2019-05-27 23:16:22 +0200

    ogp meta tags moved to public

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,23 +9,6 @@ const SEO = () => (
       name="description"
       content="Najlepsza wyszukiwarka tekstów utworów autorstwa Grimes"
     />
-    <meta property="og:url" content="https://gurajmesu-search.netlify.com/" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Gurajmesu Search" />
-    <meta
-      property="og:description"
-      content="Najlepsza wyszukiwarka tekstów utworów autorstwa Grimes"
-    />
-    <meta
-      property="og:image"
-      content="http://images3.alphacoders.com/630/630823.jpg"
-    />
-    <meta
-      property="og:image:secure_url"
-      content="https://images3.alphacoders.com/630/630823.jpg"
-    />
-    <meta property="og:image:width" content="1920" />
-    <meta property="og:image:height" content="1080" />
   </Helmet>
 );
 
